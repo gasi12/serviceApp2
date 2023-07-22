1f7069c016bb1c21b5d32e64aa3987b2f6032fdb
pawel <gussy1258@gmail.com>
2023-07-22 19:15:15 +0200

    cleanup

---
+++ src/main/java/com/example/serviceApp/chat/ChatTicket.java
@@ -0,0 +1,11 @@
+package com.example.serviceApp.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class ChatTicket {
+    private String ticket;
+}
