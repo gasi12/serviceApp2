c253a5e5438ec0e4908aef50721839f6a4fa2892
gasi12 <gussy1258@gmail.com>
2023-07-22 10:28:22 +0200

    cleanup

--- src/main/java/com/example/serviceApp/chat/GreetingController.java
+++ src/main/java/com/example/serviceApp/chat/GreetingController.java
@@ -1,22 +1,14 @@
 package com.example.serviceApp.chat;
 
-import com.example.serviceApp.security.config.JwtService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.util.HtmlUtils;
 
-import java.security.Principal;
 import java.util.Map;
 
 @Controller
