9d88033509b940864ba170823b662dbce6596a60
trelesrele123@gmail.com <trelesrele123@gmail.com>
2023-07-24 19:00:10 +0200

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMTcwNQ/spectral.js?branch_slug=master&token=b36598ad-fbc9-4a2e-9164-b5a1367ed0ae";
+export default { extends: ruleset };
