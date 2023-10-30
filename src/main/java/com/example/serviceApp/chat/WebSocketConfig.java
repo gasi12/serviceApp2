184db2aa72de1a165eb8f4ee5d0d3fcece67369b
Pawel <gussy1258@gmail.com>
2023-10-31 00:37:03 +0100

    broke it

--- src/main/java/com/example/serviceApp/chat/WebSocketConfig.java
+++ src/main/java/com/example/serviceApp/chat/WebSocketConfig.java
@@ -3,21 +3,34 @@ package com.example.serviceApp.chat;
 
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final TicketService ticketService;
+
+
+
+    public WebSocketConfig(TicketService ticketService) {
+        this.ticketService = ticketService;
+
+    }
 
     @Override
-    public void registerStompEndpoints( StompEndpointRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:3000")
-                .setAllowedOriginPatterns("*").withSockJS().setInterceptors(new TicketHandshakeInterceptor());
+                .setAllowedOriginPatterns("*").withSockJS().setInterceptors(new TicketHandshakeInterceptor(ticketService));
     }
 
     @Override
