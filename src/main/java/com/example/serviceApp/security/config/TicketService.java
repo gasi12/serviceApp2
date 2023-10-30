9754c949e2feb87d0edbc001efb9aa138fa97d6b
Pawel <gussy1258@gmail.com>
2023-10-31 00:36:52 +0100

    deleted

--- src/main/java/com/example/serviceApp/security/config/TicketService.java
+++ src/main/java/com/example/serviceApp/security/config/TicketService.java
@@ -1,23 +1,23 @@
-package com.example.serviceApp.security.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-@Slf4j
-@Service
-public class TicketService {
-    private final ConcurrentHashMap<String, Boolean> tickets = new ConcurrentHashMap<>();
-
-    public String createTicket() {
-        String ticket = UUID.randomUUID().toString();
-        tickets.put(ticket, true); // The ticket is valid once created
-      log.info("Created ticket "+ticket);
-        return ticket;
-    }
-
-    public boolean validateAndInvalidateTicket(String ticket) {
-        return tickets.remove(ticket) != null;  // The ticket can only be used once
-    }
-}
+//package com.example.serviceApp.security.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.UUID;
+//import java.util.concurrent.ConcurrentHashMap;
+//@Slf4j
+//@Service
+//public class TicketService {
+//    private final ConcurrentHashMap<String, Boolean> tickets = new ConcurrentHashMap<>();
+//
+//    public String createTicket() {
+//        String ticket = UUID.randomUUID().toString();
+//        tickets.put(ticket, true); // The ticket is valid once created
+//      log.info("Created ticket "+ticket);
+//        return ticket;
+//    }
+//
+//    public boolean validateAndInvalidateTicket(String ticket) {
+//        return tickets.remove(ticket) != null;  // The ticket can only be used once
+//    }
+//}
